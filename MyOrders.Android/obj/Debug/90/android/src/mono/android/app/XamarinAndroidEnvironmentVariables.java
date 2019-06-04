db137fdf5877d4760e091c48132f412cfe417cf1
Aaron <aaron_perez11@outlook.com>
2019-06-03 20:33:28 -0500

    Initial commit

---
+++ MyOrders.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "253af40e-8b97-4429-82d5-68009515e312",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
